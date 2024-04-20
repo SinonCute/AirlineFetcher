f855481ef32d72ba5311d798de2f81790a06a051
SinonCute <caothanhhien62@gmail.com>
2024-04-20 02:00:02 -0700

    love kotlin & java

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.7")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,98 @@
 package hiencao.me
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+import okhttp3.OkHttpClient
+import java.io.File
+
 
 fun main() {
     val airlinesCodes: List<String> = listOf("ASA", "AYA", "AAL", "VXP", "MXY", "DAL", "EAL", "FFT", "HAL", "JBU",
         "RVF", "SWA", "NKS", "SCX", "UAL")
-    val aircrafts: List<Aircraft> = listOf()
+    calculateJson(airlinesCodes)
 
-    val times = mutableListOf<String>()
+}
+private fun calculateJson(airlinesCodes: List<String>) {
+    val currentPath = System.getProperty("user.dir")
+    val aircrafts = mutableListOf<Aircraft>()
 
-    // Generate all possible times
-    for (hour in 0..23) {
-        for (minute in 0..59 step 5) {
-            for (second in 0..55 step 5) {
-                val time = "$hour".padStart(2, '0') + "$minute".padStart(2, '0') + "$second".padStart(2, '0')
-                if (minute == 55 && second == 0) {
-                    times.add(time)
+    for (month in 1..12) {
+        val monthFormatted = "$month".padStart(2, '0')
+        val file = File("$currentPath/${monthFormatted}.json")
+        val json = file.readText()
+        val aircraftsArray = Gson().fromJson(json, Array<Aircraft>::class.java)
+        aircrafts.addAll(aircraftsArray)
+    }
+
+    val groupedAircrafts = aircrafts.groupBy { it.icao }
+
+    val totalFlights = aircrafts.size
+    val totalAirLines = groupedAircrafts.size
+    val totalFlightsPerAirline = groupedAircrafts.map { it.value.size }
+
+    println("Total flights: $totalFlights")
+    println("Total airlines: $totalAirLines")
+    println("Total flights per airline:")
+    totalFlightsPerAirline.forEachIndexed { index, value ->
+        println("${airlinesCodes[index]}: $value")
+    }
+}
+
+private fun downloadAircrafts(airlinesCodes: List<String>) {
+    val aircrafts = mutableListOf<Aircraft>()
+
+    val client = OkHttpClient()
+
+    //timer start
+    val start = System.currentTimeMillis()
+    for (month in 1..12) {
+        val times = mutableListOf<String>()
+
+        for (hour in 0..24) {
+            var time = "${hour.toString().padStart(2, '0')}0000"
+            if (hour == 24) {
+                time = "235955"
+            }
+            times.add(time)
+        }
+
+        val monthFormatted = "$month".padStart(2, '0')
+
+        times.forEach { time ->
+            println("Downloading data for ${monthFormatted}/01/${time}Z...")
+            val url = "https://samples.adsbexchange.com/readsb-hist/2021/${monthFormatted}/01/${time}Z.json.gz"
+            val response = client.newCall(okhttp3.Request.Builder().url(url).build()).execute()
+
+            if (response.code != 200) {
+                println("Failed to download data for ${monthFormatted}/01/${time}Z")
+                return@forEach
+            }
+
+            val jsonObject = Gson().fromJson(response.body?.string(), JsonObject::class.java)
+            val aircraftsArray = jsonObject.getAsJsonArray("aircraft").filter { it.asJsonObject.get("flight") != null }
+            response.close()
+            aircraftsArray.forEach {
+                val flight = it.asJsonObject.get("flight").asString
+                airlinesCodes.forEach { code ->
+                    if (flight.startsWith(code) && aircrafts.any { aircraft -> aircraft.flight == flight }.not()) {
+                        aircrafts.add(Aircraft(code, flight.trim()))
+                    }
                 }
             }
         }
+
+        println("Saving data to ${monthFormatted}.json...")
+        val currentPath = System.getProperty("user.dir")
+        val file = File("$currentPath/${monthFormatted}.json")
+
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        val json = gson.toJson(aircrafts)
+        file.writeText(json)
     }
-
-
-
-
+    //timer end
+    val end = System.currentTimeMillis()
+    println("Time taken: ${(end - start) / 60000}m")
 }
 
 data class Aircraft(
